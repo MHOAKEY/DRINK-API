9a876bb5fdd0ed1dcd08f40ec7dab2b5e220d7d6
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-19 14:27:48 -0400

    Built function to render the drink data into the inner html.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const drinkContainer = document.getElementById("drinkContainer");
+
 function getDrinkData() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then((response) => response.json())
-    .then((jsonResponse) => console.log(jsonResponse));
+    .then((jsonResponse) => {
+      renderDrinkData(jsonResponse.drinks[0]);
+      console.log(jsonResponse.drinks[0]);
+    });
+}
+
+function renderDrinkData(drink) {
+  drinkContainer.innerHTML = drink.strDrink;
 }
