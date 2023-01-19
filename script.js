e8dda17137dfcb717d6d54d432831d92133857a9
Matt Oake <matt@Matts-MacBook-Air.local>
2023-01-19 15:07:31 -0400

    bold ingredients

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function renderDrinkData(drink) {
   let innerHTMLStr = `<h2>${drink.strDrink} (${drink.strAlcoholic})</h2>`;
   innerHTMLStr += `<h3>${drink.strCategory}</h3>`;
   innerHTMLStr += `<ul>
-  <li>${drink.strMeasure1} ${drink.strIngredient1}</li>
-  <li>${drink.strMeasure2} ${drink.strIngredient2}</li>
-  <li>${drink.strMeasure3} ${drink.strIngredient3}</li>
-  <li>${drink.strMeasure4} ${drink.strIngredient4}</li>
-  <li>${drink.strMeasure5} ${drink.strIngredient5}</li>
+  <li>${drink.strMeasure1} <b>${drink.strIngredient1}</b></li>
+  <li>${drink.strMeasure2} <b>${drink.strIngredient2}</b></li>
+  <li>${drink.strMeasure3} <b>${drink.strIngredient3}</b></li>
+  <li>${drink.strMeasure4} <b>${drink.strIngredient4}</b></li>
+  <li>${drink.strMeasure5} <b>${drink.strIngredient5}</b></li>
   </ul>`;
   innerHTMLStr += `<p><i>${drink.strInstructions}</i></p>`;
   drinkContainer.innerHTML = innerHTMLStr;
